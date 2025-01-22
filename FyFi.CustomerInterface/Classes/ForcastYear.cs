cb7de6113d5571fbb47d1993186d9aa5f09ca89f
Julie <lam.julie@hotmail.com>
2025-01-23 00:23:32 +1100

    FYFI-3 - initial commit with monthly capture

---
+++ FyFi.CustomerInterface/Classes/ForcastYear.cs
@@ -0,0 +1,13 @@
+﻿namespace FyFi.CustomerInterface.Classes
+{
+    public class ForcastYear
+    {
+        public int Year { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Super { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal Savings { get; set; }
+        //public decimal SavingsAccrued { get; set; }
+        //public decimal SuperAccrued { get; set; }
+    }
+}
