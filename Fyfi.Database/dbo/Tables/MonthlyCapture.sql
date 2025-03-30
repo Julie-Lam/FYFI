8cd585a7d96bd4bb23ac88a25a0a6c321e9b6e62
Julie <lam.julie@hotmail.com>
2025-03-30 13:32:18 +1100

    FYFI-3 imported dacpac file into project

---
+++ Fyfi.Database/dbo/Tables/MonthlyCapture.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[MonthlyCapture] (
+    [MonthlyCaptureId]   INT      IDENTITY (1, 1) NOT NULL,
+    [MonthlyCaptureDate] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([MonthlyCaptureId] ASC)
+);
+
---
+++ Fyfi.Database/dbo/Tables/MonthlyCaptureItem.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MonthlyCaptureItem] (
+    [MonthlyCaptureItemId] INT           IDENTITY (1, 1) NOT NULL,
+    [MonthlyCaptureId]     INT           NULL,
+    [ItemName]             VARCHAR (MAX) NULL,
+    [ItemAmount]           DECIMAL (18)  NULL,
+    PRIMARY KEY CLUSTERED ([MonthlyCaptureItemId] ASC),
+    FOREIGN KEY ([MonthlyCaptureId]) REFERENCES [dbo].[MonthlyCapture] ([MonthlyCaptureId])
+);
+
