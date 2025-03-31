39c44b98f3b51b8399435e63bfcc5d9315ce61fc
Julie <lam.julie@hotmail.com>
2025-04-01 09:37:51 +1100

    FYFI-3 - installed EF Core for sql server
    TODO:
    - Reverse engineer the db to the ef core models
    - Successfully perform queries via ef core
    - Refactor the connectionstring out of being hardcoded in logic

---
+++ FyFi.Infrastructure/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FyFi.Infrastructure
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        //public List<Role> Roles { get; private set; }
+    }
+}
