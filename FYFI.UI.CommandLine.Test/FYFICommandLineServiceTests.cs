a83b7eabff4fcf2eb345df752e242413475fed15
Julie <lam.julie@hotmail.com>
2026-04-30 22:43:18 +1000

    added tests

--- FYFI.UI.CommandLine.Test/FYFICommandLineServiceTests.cs
+++ FYFI.UI.CommandLine.Test/FYFICommandLineServiceTests.cs
@@ -31,25 +31,40 @@ namespace FYFI.UI.CommandLine.Test
             var mockRepoService = Substitute.For<IFYFIRepositoryService>();
 
             fyfiCommandLineService._consoleUserInterfaceService = mockUIService;
-            fyfiCommandLineService._repository = mockRepoService; 
+            fyfiCommandLineService._repository = mockRepoService;
 
+
+            FiOutlook actualFiOutlook = null;
+
+
+            // using When...Do to capture args
+            mockUIService
+              .When(mock => mock.PrintFinancialOutlookDetails(Arg.Any<FiOutlook>()))
+              .Do(callInfo => actualFiOutlook = callInfo.Arg<FiOutlook>());
 
             //Act
             fyfiCommandLineService.HandleNewOutlook();
 
 
             //Assert
-            var expectedOutlook = new FiOutlook()
-            {
-                FiOutlookYears = new List<FiOutlookYear>() 
-                { 
-                    new FiOutlookYear() {YearDate = DateTime.Now.AddYears(1), Cash = 12000, SavingsYearly = 12000}, 
-                    new FiOutlookYear() {YearDate = DateTime.Now.AddYears(2), Cash = 24000, SavingsYearly = 12000},            
-                    new FiOutlookYear() {YearDate = DateTime.Now.AddYears(3), Cash = 36000, SavingsYearly = 12000}  
-                }
-            };
 
-            mockUIService.Received().PrintFinancialOutlookDetails(expectedOutlook); 
+            Assert.That(actualFiOutlook.FiOutlookYears, Is.Not.Null);
+            Assert.That(actualFiOutlook.FiOutlookYears.Count, Is.EqualTo(3));
+
+            //1st outlook year
+            Assert.That(actualFiOutlook.FiOutlookYears[0].YearDate.Year, Is.EqualTo(DateTime.Now.AddYears(1).Year));
+            Assert.That(actualFiOutlook.FiOutlookYears[0].Cash, Is.EqualTo(12000));
+            Assert.That(actualFiOutlook.FiOutlookYears[0].SavingsYearly, Is.EqualTo(12000));
+
+            //2st outlook year
+            Assert.That(actualFiOutlook.FiOutlookYears[1].YearDate.Year, Is.EqualTo(DateTime.Now.AddYears(2).Year));
+            Assert.That(actualFiOutlook.FiOutlookYears[1].Cash, Is.EqualTo(24000));
+            Assert.That(actualFiOutlook.FiOutlookYears[1].SavingsYearly, Is.EqualTo(12000));
+
+            //1st outlook year
+            Assert.That(actualFiOutlook.FiOutlookYears[2].YearDate.Year, Is.EqualTo(DateTime.Now.AddYears(3).Year));
+            Assert.That(actualFiOutlook.FiOutlookYears[2].Cash, Is.EqualTo(36000));
+            Assert.That(actualFiOutlook.FiOutlookYears[2].SavingsYearly, Is.EqualTo(12000));
         }
     }
 }
